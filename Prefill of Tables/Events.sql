e0992d622a6666ba5315b228560455ab9cc1b85e
onederey <morozovcustom@yandex.ru>
2022-05-13 00:00:42 +0400

    Added prefill

--- Prefill of Tables/Events.sql
+++ Prefill of Tables/Events.sql
@@ -1,39 +1,82 @@
+-----------------------------------------------------------------
 SET IDENTITY_INSERT [dbBankGM].[dbo].[Events] ON
 GO
 
-INSERT INTO [dbBankGM].[dbo].[Events] (EventID, [Description])
-VALUES
-    (0, 'Trace'),
-    (1, 'UserAction')
+IF NOT EXISTS(SELECT 1 FROM [dbo].[Events])
+BEGIN
+    INSERT INTO [dbBankGM].[dbo].[Events] (EventID, [Description])
+    VALUES
+        (0, 'Trace'),
+        (1, 'UserAction')
+END
 GO
 
 SET IDENTITY_INSERT [dbBankGM].[dbo].[Events] OFF
 GO
 
+-----------------------------------------------------------------
 
 SET IDENTITY_INSERT [dbBankGM].[dbo].[Users] ON
 GO
-
-INSERT INTO [dbBankGM].[dbo].[Users] (Id, Username, Privileges, PassHash)
-VALUES
-    (-100, 'TaskService', 'ServicePrivelege', 'NO PASS')
+IF NOT EXISTS(SELECT 1 FROM [dbo].[Users])
+BEGIN
+    INSERT INTO [dbBankGM].[dbo].[Users] (Id, Username, Privileges, PassHash)
+    VALUES
+        (-100, 'TaskService', 'ServicePrivelege', 'NO PASS')
+END
 GO
 
 SET IDENTITY_INSERT [dbBankGM].[dbo].[Users] OFF
 GO
 
--- Task CBR
-INSERT INTO dbo.ServiceTasks 
-    (TaskID, [IsEnabled],[TaskType],[Branch],
-     [TaskName],[LastWorkTime],[TaskStartTime],
-     [TaskEndTime],[Dependency],[FilePath],
-     [FieldsCount],[FieldsSeparator],[Params],
-     [ModifiedBy],[AuthoriziedBy],[Description],[ManualStart])
-VALUES
-    (0, 1, 1, 0,
-     'CurrentCBRRates', null, '2022-05-01 01:00:00',
-     '2022-05-01 23:59:00', '', null,
-     null, null, null,
-     'Andrej', 'Andrej', 'CBR Rates', 0)
+IF NOT EXISTS (SELECT 1 FROM dbo.TaskTypes)
+BEGIN
 
+    INSERT INTO dbo.Events ([Description])
+    VALUES ('WebService'),
+        ('StoredProcedure'),
+        ('File')
 
+END
+
+-----------------------------------------------------------------
+
+-- Task CBR RATES
+IF NOT EXISTS(SELECT 1 FROM dbo.ServiceTasks WHERE TaskName = 'CurrentCBRRates')
+BEGIN
+    INSERT INTO dbo.ServiceTasks 
+        (TaskID, [IsEnabled],[TaskType],[Branch],
+        [TaskName],[LastWorkTime],[TaskStartTime],
+        [TaskEndTime],[Dependency],[FilePath],
+        [FieldsCount],[FieldsSeparator],[Params],
+        [ModifiedBy],[AuthoriziedBy],[Description],[ManualStart])
+    VALUES
+        (0, 1, 1, 0,
+        'CurrentCBRRates', null, '2022-05-01 01:00:00',
+        '2022-05-01 23:59:00', '', null,
+        null, null, null,
+        'Andrej', 'Andrej', 'CBR Rates', 0)
+END
+GO
+-----------------------------------------------------------------
+
+-- Task ED Banks
+
+IF NOT EXISTS(SELECT 1 FROM dbo.ServiceTasks WHERE TaskName = 'EDBanks')
+BEGIN
+    INSERT INTO dbo.ServiceTasks 
+        (TaskID, [IsEnabled],[TaskType],[Branch],
+        [TaskName],[LastWorkTime],[TaskStartTime],
+        [TaskEndTime],[Dependency],[FilePath],
+        [FieldsCount],[FieldsSeparator],[Params],
+        [ModifiedBy],[AuthoriziedBy],[Description],[ManualStart], [Url])
+    VALUES
+        (1, 1, 1, null,
+        'EDBanks', null, '2022-05-01 01:00:00',
+        '2022-05-01 23:59:00', '', 'C:\TEST',
+        null, null, null,
+        'Andrej', 'Andrej', 'BIC Dictionary', 0, 'https://www.cbr.ru/s/newbik')
+END
+GO
+
+-----------------------------------------------------------------
